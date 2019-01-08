5d1931c4855db177df22da33eeb70b924c3a7a3f
DESKTOP-LTMA4Q9\dingz <dingzebin0@qq.com>
2019-01-08 14:02:36 +0800

    Given a string, find the length of the longest substring without repeating characters.

---
+++ src/leetcode/algorithms/lengthOfLongestSubstring/LengthOfLongestSubstring.java
@@ -0,0 +1,23 @@
+package leetcode.algorithms.lengthOfLongestSubstring;
+
+/**
+ *  Given a string, find the length of the longest substring without repeating characters.
+ */
+public class LengthOfLongestSubstring {
+    public static void main(String[] args) {
+      String s = "abba";
+      System.out.println(new LengthOfLongestSubstring().lengthOfLongestSubstring(s));
+    }
+    public int lengthOfLongestSubstring(String s) {
+        int[] arr = new int[128];
+        int max = 0;
+        for (int i = 0, j = 0;i < s.length(); i++) {
+            if (arr[s.charAt(i)] != 0) {
+                j = Math.max(j, arr[s.charAt(i)]);
+            }
+            arr[s.charAt(i)] = i + 1;
+            max = Math.max(max, i - j + 1);
+        }
+        return max;
+    }
+}
