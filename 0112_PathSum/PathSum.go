3e00b60100d257609937cebae82c047267bc9a3f
dzeb <dingzebin0@gmail.com>
2022-01-06 12:47:53 +0800

    0112_PathSum

---
+++ 0112_PathSum/PathSum.go
@@ -0,0 +1,26 @@
+package main
+
+func main() {
+
+}
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func hasPathSum(root *TreeNode, targetSum int) bool {
+	if root == nil {
+		return false
+	}
+	if root.Left == nil && root.Right == nil {
+		if targetSum == root.Val {
+			return true
+		} else {
+			return false
+		}
+	} else {
+		return hasPathSum(root.Left, targetSum-root.Val) || hasPathSum(root.Right, targetSum-root.Val)
+	}
+}
